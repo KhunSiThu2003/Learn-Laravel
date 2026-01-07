a31222b8135daecb0b66a26a16be3b2eca116e08
KhunSiThu2003 <khunsithu350@gmail.com>
2026-01-07 23:30:20 +0630

    init

---
+++ laravel-api-course-2025/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\V1\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('v1')->group(function() {
+        Route::apiResource('posts', PostController::class);
+    });
+});
+
+
+
+require __DIR__.'/auth.php';
